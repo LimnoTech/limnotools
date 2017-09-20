2d1b576ade25def13a3b8e3d844b1bd7824244d8
padilla410 <padilla410@gmail.com>
2017-09-20 14:44:50 -0400

    Update to nd_remover
    Simplification of nd_remover code. Removing dependence on stringr.

--- R/nd_remover.R
+++ R/nd_remover.R
@@ -4,20 +4,21 @@
 #'
 #' @param x a character vector values
 #'
-#' @import lubridate magrittr stringr
-#'
 #' @export
 #'
 #' @details This function removes the "<" flag from the front a sampling result value.
 #' Currently, this function is used in conjunction with \code{nd_classifier}
 #'
+#' @examples
+#' \dontrun{
+#' x <- c('0.24', '2', '0.5', '<2.5', '<6')
+#' nd_remover(x)
+#' }
 #' @return Returns a numeric vector
 #'
 #' @seealso \code{nd_classifier}
 #'
 nd_remover <- function(x){
-  ifelse(str_detect(x, '<')
-         , as.numeric(str_replace(x, "[<]", ""))
-         , x <- as.numeric(x)
-  )
+  gsub('<', '', x, fixed = T)
 }
+

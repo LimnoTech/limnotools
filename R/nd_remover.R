1803b40d4c5ebce44c9ed6bd84afb98ed020c5e7
padilla410 <padilla410@gmail.com>
2017-09-21 09:23:44 -0400

    Updating examples in nd functions
    Adding ability to handle oddly formatted values

--- R/nd_remover.R
+++ R/nd_remover.R
@@ -4,6 +4,7 @@
 #'
 #' @param x a character vector values
 #' @param halfDL logical, substitute half of the detection limit? Default is \code{FALSE}
+#' @param ... additional arguments passed to other methods
 #'
 #' @export
 #'
@@ -12,17 +13,21 @@
 #'
 #' @examples
 #' \dontrun{
-#' x <- c('0.24', '2', '>0.5', '<2.5', '<6')
+#' x <- c('0.24', '>0.25', '2', '>0.5', '<2.5', '<6')
 #' nd_remover(x)
-#' nd_remover(x)
+#'
+#' x <- c('  0.24', '> 0.25', '2 ', '>.5', '<2.5', '<  6')
+#' nd_remover(x, which = 'both')
 #' }
 #'
 #' @return Returns a numeric vector
 #'
-#' @seealso \code{nd_classifier}
+#' @seealso \code{nd_classifier}, \code{\link[base]{trimws}}
 #'
-nd_remover <- function(x){
-  # signs <- c([<>])
+nd_remover <- function(x, halfDL = FALSE, ...){
+
+  x <- trimws(x, ...)
+
   x <- gsub('>', '', x, fixed = T)
   x <- as.numeric(gsub('<', '', x, fixed = T))
 

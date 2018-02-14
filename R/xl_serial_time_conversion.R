9c6f7accb84a98def0402fc45d607f41d744a234
padilla410 <padilla410@gmail.com>
2018-02-14 12:39:43 -0500

    adding xl_serial_time_conversion

---
+++ R/xl_serial_time_conversion.R
@@ -0,0 +1,35 @@
+#' Excel Serial Time Conversion
+#'
+#' Create an empty US base map in ggplot
+#'
+#' @param x num serial date
+#' @param unit a character string specifying a time unit or a multiple of a unit to be rounded to. Valid base units are second, minute, hour, day, week, month, bimonth, quarter, season, halfyear and year. Arbitrary unique English abbreviations as in the \code{\link[lubridate]{period()}} constructor are allowed. Rounding to multiple of units (except weeks) is supported. If no unit is specified, unit defaults to "minute".
+#'
+#' @importFrom lubridate round_date
+#'
+#' @export
+#'
+#' @details Converts serial dates from excel into POSIXct dates
+#'
+#' @examples
+#' \dontrun{
+#' xl_serial_time_conversion(40544.86)
+#' }
+#'
+#' @return Returns a \code{\link[ggplot2]{ggplot}} object
+#'
+#' @author Julie Padilla
+#'
+#' @concept Conversion
+#'
+xl_serial_time_conversion <- function(x, unit = NA){
+  dt <- as.POSIXct(x * (60*60*24)
+                   , origin = "1899-12-30"
+                   , tz = "UTC")
+
+  if(!is.na(unit)){
+    dt <- round_date(dt, unit = unit)
+  }
+
+  return(dt)
+}
